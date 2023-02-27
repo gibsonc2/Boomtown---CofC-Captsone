3d907c1c4b9a47bdd5cdb58dc7335c4d3f534e8b
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-02-26 19:46:08 -0500

    Communication Preferences
    Communication preferences are now received from the frontend and put into the database.
    0 = do not contact
    1 = contact by email only
    2 = contact by text only
    3 = contact by email or text

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/signin", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        add_to_db(boomtown, request.form.get("fname"), request.form.get("lname"), request.form.get("email"), request.form.get("phone"))
-    return render_template("SignIn.html")
+        commPrefs = getCommPrefs(request.form.get('prefEmail'), request.form.get('prefText'), request.form.get('prefNone'))
+        add_to_db(
+            boomtown, 
+            request.form.get("fname"), 
+            request.form.get("lname"), 
+            request.form.get("email"), 
+            request.form.get("phone"), 
+            commPrefs
+        )
+    return redirect("http://localhost:3000")
 
 def connect_to_db():
     boomtown = mysql.connector.connect(
@@ -18,9 +26,19 @@ def connect_to_db():
     )
     return boomtown
     
-def add_to_db(db, fname, lname, email, phone):
+def add_to_db(db, fname, lname, email, phone, commPref):
     c = db.cursor()
-    c.execute(f"INSERT INTO Guest (guestFName, guestLName, guestEmail, guestPhone) VALUES ('{fname}', '{lname}', '{email}', '{phone}')")
+    c.execute(f"INSERT INTO Guest (guestFName, guestLName, guestEmail, guestPhone, guestCommPref) VALUES ('{fname}', '{lname}', '{email}', '{phone}', {commPref})")
     db.commit()
+    
+def getCommPrefs(einput, tinput, ninput):
+    if ninput == 'on':
+        return 0 # do not contact
+    elif einput == 'on' and tinput is None:
+        return 1 # contact by email only
+    elif einput is None and tinput == 'on':
+        return 2 # contact by text only
+    else:
+        return 3 # contact by text or email
       
 boomtown = connect_to_db()
