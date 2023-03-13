0b840c566a3f18e59f8d26251e482f14608b709f
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-13 12:36:21 -0400

    Moved methods to btutils.py
    Moved connect_to_db() and add_to_db() to separate module to improve organization of backend code

--- openhouse/app.py
+++ openhouse/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from btutils import connect_to_db, add_to_db
 import mysql.connector
 
 app = Flask(__name__)
@@ -6,16 +7,19 @@ app = Flask(__name__)
 @app.route("/signin", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
+        db = connect_to_db()
         commPrefs = getCommPrefs(request.form.get('prefEmail'), request.form.get('prefText'), request.form.get('prefNone'))
         add_to_db(
-            boomtown, 
+            db, 
             request.form.get("fname"), 
             request.form.get("lname"), 
             request.form.get("email"), 
             request.form.get("phone"), 
             commPrefs
         )
+        db.close()
     return redirect("http://localhost:3000")
+
 
 @app.route("/adminPortal", methods=["GET", "POST"])
 def adminPortal():
@@ -37,21 +41,8 @@ def adminPortal():
     # close cursor and db connection
     c.close()
     db.close()
-
-def connect_to_db():
-    boomtown = mysql.connector.connect(
-        host="database-1.cluster-cwnezg0iyvk9.us-east-1.rds.amazonaws.com",
-        user="admin",
-        password="boomtown",
-        database="BoomTown"
-    )
-    return boomtown
-    
-def add_to_db(db, fname, lname, email, phone, commPref):
-    c = db.cursor()
-    c.execute(f"INSERT INTO Guest (guestFName, guestLName, guestEmail, guestPhone, guestCommPref) VALUES ('{fname}', '{lname}', '{email}', '{phone}', {commPref})")
-    db.commit()
-    
+   
+# temporary solution
 def getCommPrefs(einput, tinput, ninput):
     if ninput == 'on':
         return 0 # do not contact
@@ -61,5 +52,3 @@ def getCommPrefs(einput, tinput, ninput):
         return 2 # contact by text only
     else:
         return 3 # contact by text or email
-      
-boomtown = connect_to_db()
