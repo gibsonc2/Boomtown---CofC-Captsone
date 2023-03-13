87e3e7ef2e7232aa19a8f2eaf70e27d27ea5c9ae
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-13 12:37:18 -0400

    Add files via upload

---
+++ openhouse/btutils.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+def connect_to_db():
+    db = mysql.connector.connect(
+        host="database-1.cluster-cwnezg0iyvk9.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="boomtown",
+        database="BoomTown"
+    )
+    return db
+
+def add_to_db(db, fname, lname, email, phone, commPref):
+    c = db.cursor()
+    c.execute(f"INSERT INTO Guest (guestFName, guestLName, guestEmail, guestPhone, guestCommPref) VALUES ('{fname}', '{lname}', '{email}', '{phone}', {commPref})")
+    db.commit()
