dab27feda13d4150fccc3b5df2d6fecf8f16ed14
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-19 01:00:37 -0400

    Finished signin, register, and display for admin portal

--- openhouse/app.py
+++ openhouse/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, redirect
-from btutils import connect_to_db, add_to_db
+from flask import Flask, render_template, request, redirect, url_for, session
+from btutils import connect_to_db, add_guest, update_agent, add_agent, match_agent_info
 import mysql.connector
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(12).hex()
+# app.config['SESSION_PERMANENT'] = False
 
-@app.route("/signin", methods=["GET", "POST"])
+
+# working
+@app.route("/guestSignin", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         db = connect_to_db()
         commPrefs = getCommPrefs(request.form.get('prefEmail'), request.form.get('prefText'), request.form.get('prefNone'))
-        add_to_db(
+        add_guest(
             db, 
             request.form.get("fname"), 
             request.form.get("lname"), 
@@ -21,27 +26,84 @@ def home():
     return redirect("http://localhost:3000")
 
 
-@app.route("/adminPortal", methods=["GET", "POST"])
-def adminPortal():
+# working
+@app.route("/adminPortalDisplay", methods=["GET", "POST"])
+def adminPortalDisplay():
     db = connect_to_db()  # establish database connection
     c = db.cursor()  # create cursor for interacting with db
-    # query db
-    c.execute("SELECT agentFName, agentLName, agentPhone, agentEmail, logoPath, agentImgPath FROM Agent")
-    items = c.fetchall()  # get every item from query in tuple
+    
+    fname = session.get('fname')
+    lname = session.get('lname')
+    phone = session.get('phone')
+    email = session.get('email')
+    
     # return items to react server
     # items[0] is the entire tuple retrieved from query, so we need to index further into that
-    return {
-        "fname":items[0][0], 
-        "lname":items[0][1],
-        "phone":items[0][2], 
-        "email":items[0][3],
-        "logo": items[0][4],
-        "img": items[0][5]
-    }
+    # need to also look into how to pull and send images to react
     # close cursor and db connection
     c.close()
     db.close()
+    
+    return {
+        "fname":fname, 
+        "lname":lname,
+        "phone":phone, 
+        "email":email,
+        "logo": "logo",
+        "img": "img"
+    }
    
+
+# fix
+@app.route("/adminPortalUpdate", methods=["GET", "POST"])
+def adminPortalUpdate():
+    db = connect_to_db()
+    # also need to receive images if updated
+    update_agent(
+        db, 
+        request.form.get("fname"), 
+        request.form.get("lname"), 
+        request.form.get("email"), 
+        request.form.get("phone")
+    )
+    db.close()
+    return redirect("http://localhost:3000/admindisplay")
+
+
+# working
+@app.route("/adminPortalRegister", methods=["GET", "POST"])
+def adminPortalRegister():
+    session['fname'] = request.form.get("fname")
+    session['lname'] = request.form.get("lname")
+    session['email'] = request.form.get("email")
+    session['phone'] = request.form.get("phone")
+    passwd = request.form.get("passwd")
+    db = connect_to_db()
+    add_agent(db, session['fname'], session['lname'], session['email'], session['phone'], passwd) # hash password before putting into db
+    db.close()
+    # add check for duplicate email, phone, etc
+    return redirect('http://localhost:3000/admindisplay')
+
+
+# working
+@app.route("/adminPortalSignin", methods=["GET", "POST"])
+def adminPortalSignin():
+    db = connect_to_db()
+    ret_val = match_agent_info(db, request.form.get("email"), request.form.get("passwd")) # hash input password in final version
+    info = ret_val[1]
+    # if user/pw pair found in db, e will be ("SUCCESS", <agentID>)
+    # else, e will be ("FAIL", -1)
+    if ret_val[0] == "SUCCESS":
+        session['aID'] = info['aID']
+        session['fname'] = info['fname']
+        session['lname'] = info['lname']
+        session['email'] = info['email']
+        session['phone'] = info['phone']
+        return redirect('http://localhost:3000/admindisplay')
+    else:
+        return redirect('http://localhost:3000/adminsignin')
+    
+    
 # temporary solution
 def getCommPrefs(einput, tinput, ninput):
     if ninput == 'on':
@@ -52,3 +114,4 @@ def getCommPrefs(einput, tinput, ninput):
         return 2 # contact by text only
     else:
         return 3 # contact by text or email
+
