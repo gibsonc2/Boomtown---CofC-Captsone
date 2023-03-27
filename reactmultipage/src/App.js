e7ad76c93f18f03543747ea529915041a6b5906a
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-26 21:54:03 -0400

    Update App.js

--- reactmultipage/src/App.js
+++ reactmultipage/src/App.js
@@ -6,6 +6,8 @@ import GuestSignin from './pages/GuestSignin'
 import AdminRegister from './pages/AdminRegister'
 import AdminSignin from './pages/AdminSignin'
 import AdminDisplay from './pages/AdminDisplay'
+import AdminUpdate from './pages/AdminUpdate'
+import UploadPropImg from './pages/AdminUploadPropImg'
 
 function App(){
   return (
@@ -16,6 +18,8 @@ function App(){
         <Route path='adminregister' element={ <AdminRegister/> } />
         <Route path='adminsignin' element={ <AdminSignin/> } />
         <Route path='admindisplay' element={ <AdminDisplay/> } />
+        <Route path='adminupdate' element={ <AdminUpdate/> } />
+		<Route path='testupload' element={ <UploadPropImg/> } />
       </Routes>
     </div>
   )
